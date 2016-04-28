93960d7d7084a2f15db3781250ea69bcfaeafb07
jiazhenkai <jiazhenkai@renrenche.com>
2016-04-29 00:55:21 +0800

    git ignore test

--- app/src/main/java/com/renrenche/filter/MainActivity.java
+++ app/src/main/java/com/renrenche/filter/MainActivity.java
@@ -3,11 +3,16 @@ package com.renrenche.filter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.renrenche.filterlibrary.FilterLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FilterLayout mFilterLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFilterLayout = (FilterLayout) findViewById(R.id.filter);
     }
 }
